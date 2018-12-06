f2f71d5807576dac65f20b539c46c6ef77512696
hvianna <hvianna@gmail.com>
2018-12-06 20:21:48 -0200

    Added logic for fixed and floating holidays; initial support for multiple languages.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 desktopCal.js
 =============
 
-A printable desktop calendar generator built with HTML, CSS & JavaScript.
+A one-sheet desktop calendar generator built with HTML, CSS & JavaScript.
 
 https://hvianna.github.io/desktopCal.js/
 
@@ -13,7 +13,9 @@ This is Work In Progress.
 + [Loading an image from the local filesystem](https://stackoverflow.com/a/30997752/2370385)
 + [Styling an HTML file button](https://stackoverflow.com/a/25825731/2370385)
 + [Most efficient leap year test](https://stackoverflow.com/a/11595914/2370385)
++ [Easter Date in JavaScript](https://stackoverflow.com/a/44480326/2370385)
 + Photos used in the header by [@vortander](https://github.com/Vortander) and [@hvianna](https://github.com/hvianna)
++ Icons by [icons8](https://icons8.com) licensed under [Creative Commons Attribution-NoDerivs 3.0 Unported](https://creativecommons.org/licenses/by-nd/3.0/).
 
 ## License
 
--- desktopCal.js
+++ desktopCal.js
@@ -1,6 +1,6 @@
 /**
  * desktopCal.js
- * A printable desktop calendar generator built with HTML, CSS & JavaScript.
+ * A one-sheet desktop calendar generator built with HTML, CSS & JavaScript.
  *
  * https://github.com/hvianna/desktopCal.js
  *
@@ -21,6 +21,12 @@
  */
 
 
+/**
+ * Loads an image from user's computer into a calendar panel
+ *
+ * @param {HTMLInputElement object} obj    handler of the HTML file element
+ * @param {string}                  side   'top' or 'bottom' side of the calendar
+ */
 function loadImage( obj, side ) {
 
 	var reader = new FileReader();
@@ -34,12 +40,144 @@ function loadImage( obj, side ) {
 	reader.readAsDataURL( obj.files[0] );
 }
 
+/**
+ * Calculates the Easter Day
+ * https://stackoverflow.com/a/1284335/2370385
+ *
+ * @param {number} year
+ *
+ * @returns {Date object}
+ */
+function computus( year ) {
 
+	var date, a, b, c, m, d;
+
+	// Instantiate the date object
+	date = new Date;
+	date.setHours( 0, 0, 0, 0 );
+	date.setFullYear( year );
+
+	// Find the golden number
+	a = year % 19;
+
+	// Choose which version of the algorithm to use based on the given year
+	b = ( 2200 <= year && year <= 2299 ) ? ( ( 11 * a ) + 4 ) % 30 : ( ( 11 * a ) + 5 ) % 30;
+
+	// Determine whether or not to compensate for the previous step
+	c = ( ( b === 0 ) || ( b === 1 && a > 10 ) ) ? ( b + 1 ) : b;
+
+	// Use c first to find the month: April or March
+	m = ( 1 <= c && c <= 19 ) ? 3 : 2;
+
+	// Then use c to find the full moon after the northward equinox
+	d = ( 50 - c ) % 31;
+
+	// Mark the date of that full moon—the "Paschal" full moon
+	date.setMonth( m, d );
+
+	// Count forward the number of days until the following Sunday (Easter)
+	date.setMonth( m, d + ( 7 - date.getDay() ) );
+
+	// Gregorian Western Easter Sunday
+	return date;
+}
+
+/**
+ * Returns the first day that falls in the specified day of week (for floating holidays)
+ * 
+ * @param {number} dow    day of week: 0 - 6 = Sunday - Saturday
+ * @param {number} year
+ * @param {number} month
+ * @param {number} day    starting day to check
+ *
+ * @returns {number} day of month that falls in the desired day of week.
+ */
+function floatingDay( dow, year, month, day ) {
+
+	while ( ( new Date( year, month - 1, day ) ).getDay() != dow ) {
+		day++;
+	}
+
+	return day;
+}
+
+/**
+ * Checks if date is a holiday in the specified country
+ *
+ * @param {string} country
+ * @param {number} year
+ * @param {number} month
+ * @param {number} day
+ *
+ * @returns {string} "holiday" if it's a holiday, empty string otherwise.
+ */
+function checkHoliday( country, year, month, day ) {
+
+	var holidays, date, m;
+
+	switch ( country ) {
+		case 'br':
+			holidays = {
+				1: [ 1 ],
+				4: [ 21 ],
+				5: [ 1 ],
+				9: [ 7 ],
+				10: [ 12 ],
+				11: [ 2, 15 ],
+				12: [ 25 ]
+			}
+
+			// calculates floating BR holidays, based on Easter Day
+			date = computus( year );
+
+			for ( n of [ -2, -45, 107 ] ) { // Good Friday, Carnival, Corpus Christ
+
+				date.setDate( date.getDate() + n );
+
+				var m = date.getMonth() + 1;
+				if ( holidays.hasOwnProperty( m ) )
+					holidays[ m ].push( date.getDate() );
+				else
+					holidays[ m ] = [ date.getDate() ];
+			}
+
+			break;
+
+		case 'us':
+			// https://en.wikipedia.org/wiki/Federal_holidays_in_the_United_States
+			holidays = {
+				1: [ 1, floatingDay( 1, year, 1, 15 ) ],
+				2: [ floatingDay( 1, year, 2, 15 ) ],
+				5: [ floatingDay( 1, year, 5, 25 ) ],
+				7: [ 4 ],
+				9: [ floatingDay( 1, year, 9, 1 ) ],
+				10: [ floatingDay( 1, year, 10, 8 ) ], 
+				11: [ 11, floatingDay( 4, year, 11, 22 ) ],
+				12: [ 25 ]
+			}
+			break;
+	}
+
+	if ( holidays.hasOwnProperty( month ) && holidays[ month ].includes( day ) )
+		return "holiday";
+	else
+		return "";
+}
+
+/**
+ * Generates the calendar for the specified month / year
+ *
+ * @param {number} month
+ * @param {number} year
+ *
+ * @returns {string} HTML table for the calendar
+ */
 function generateCalendar( month, year ) {
 
 	var ndays = [ 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
 
-	var html, dow, prevMon,	i;
+	var html, dow, prevMon,	i, d,
+		country = 'br';
 
 	if ( ( year & 3 ) == 0 && ( ( year % 25 ) != 0 || ( year & 15 ) == 0 ) )
 		ndays[2]++; // leap year
@@ -50,11 +188,11 @@ function generateCalendar( month, year ) {
 	html = '<table><tr><th>Sun<th>Mon<th>Tue<th>Wed<th>Thu<th>Fri<th>Sat';
 	html += '<tr>'
 
-	for ( i = dow; i > 0; i-- )
-		html += '<td class="disabled">' + ( ndays[ prevMon ] - i + 1 );
+	for ( i = dow, d = ndays[ prevMon ] - i + 1; i > 0; i--, d++ )
+		html += '<td class="disabled ' + checkHoliday( country, month == 1 ? year - 1 : year, prevMon, d ) + '">' + d;
 
 	for ( i = 1; i <= ndays[ month ]; i++ ) {
-		html += '<td>' + i;
+		html += '<td class="' + checkHoliday( country, year, month, i ) + '">' + i;
 		dow++;
 		if ( dow == 7 ) {
 			html += '<tr>';
@@ -62,10 +200,11 @@ function generateCalendar( month, year ) {
 		}
 	}
 
-	i = 1;
+	d = 1;
+	month = month < 12 ? month++ : 1;
 	while ( dow > 0 && dow < 7 ) {
-		html += '<td class="disabled">' + i;
-		i++;
+		html += '<td class="disabled ' + checkHoliday( country, month == 1 ? year + 1 : year, month, d ) + '">' + d;
+		d++;
 		dow++;
 	}
 
@@ -75,7 +214,9 @@ function generateCalendar( month, year ) {
 
 }
 
-
+/**
+ * Updates the calendar preview according to the options selected by the user
+ */
 function updatePreview() {
 
 	var area = [ document.getElementById('top-half'), document.getElementById('bottom-half') ],
@@ -91,8 +232,8 @@ function updatePreview() {
 			area[ i ].querySelector('.calendar').innerHTML = generateCalendar( month[ i ], year[ i ] );
 		}
 	}
-
 }
+
 
 
 function initialize() {
@@ -124,6 +265,8 @@ function initialize() {
 	// ...and next month for calendar back
 	document.getElementById('top-year').value = year;
 	document.getElementById('top-month').selectedIndex = month;
+
+	updatePreview();
 
 }
 
